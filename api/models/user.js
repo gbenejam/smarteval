8ec0a77c96d640bd01567b6b7cedc805de4409ca
gbe <gabriela.benejam@valtech.com>
2021-04-27 17:53:11 +0200

    Login and user registration

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("user", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
   },
@@ -11,10 +12,13 @@ const User = mongoose.model("user", {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
+    trim: true,
+    lowercase: true,
     validate(value) {
       if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid.");
+        throw new Error("Email is invalid");
       }
     },
   },
@@ -34,5 +38,36 @@ const User = mongoose.model("user", {
     required: true,
   },
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+User.createIndexes();
 
 module.exports = User;
--- api/routers/user.js
+++ api/routers/user.js
@@ -1,42 +1,71 @@
-const express = require('express')
-const User = require('../models/user')
-const router = new express.Router()
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
 
 //Create user
-router.post('/users', async (req,res) => {
-    const user = new User(req.body)
-    try{
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
+//Login
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 //Get users
-router.get('/users', async (req,res) => {
-    try {
-        const users = await User.find({})
-        res.status(200).send(users)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 //Get a specific user
-router.get('/users/:id', async (req,res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send(user)
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send(user);
     }
-})
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
-module.exports = router
+module.exports = router;
