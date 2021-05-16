eccd6b6b5bbd389244a6592bbdb9017b6c215818
gbe <gabriela.benejam@valtech.com>
2021-05-16 13:34:24 +0200

    Adding features page

--- server-app/src/templates/features/features.js
+++ server-app/src/templates/features/features.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-//import { Route, Link } from 'react-router-dom';
+
+import Text from '../../components/features/text/text';
+import TextWithImage from '../../components/features/textWithImage/textWithImage';
+
+import classes from './features.module.css';
+import getData from './featuresData';
+
 
 class Features extends Component {
-    render () {
-        return (
-            <div>
-                <p>Features</p>
-            </div>
-        );
-    }
+  
+  constructor(props) {
+    super(props);
+    this.state = { features: [] };
+    this.initFeatures();
+  }
+
+  initFeatures () {
+    getData().filter(obj => obj && obj.title && obj.text)
+      .forEach(feature => this.state.features.push(feature));
+  }
+
+  render () {
+    const featuresJSX = this.state.features.map((feature, index) => {
+      if (feature.image) {
+        return <TextWithImage 
+          key={index}
+          feature={feature} />;
+      } else {
+        return <Text 
+          key={index}
+          feature={feature} />;
+      }
+    });
+
+    return (
+      <div className={classes.Features} >
+        <h1>Features</h1>
+        { featuresJSX }
+      </div>
+    );
+  }
 }
 
 export default Features;
---
+++ server-app/src/templates/features/featuresData.js
@@ -0,0 +1,45 @@
+import girlWithLaptop from '../../assets/carousel-2.jpg';
+
+
+const featuresData = () => [
+  {
+    title: "Secure from end to end",
+    text: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rutrum 
+      quis felis vulputate tristique. Integer lobortis, leo eu elementum sodales, 
+      dolor lacus ultricies velit, et condimentum dui lectus ut ex. Duis non tellus interdum, 
+      dictum ex et, faucibus nibh. Duis vitae est nec ligula aliquam varius non sed arcu. 
+      Integer faucibus ac augue sed mollis. Pellentesque nec eros quis justo venenatis rutrum. 
+      Maecenas eget augue gravida, ornare sem quis, bibendum risus. Sed mattis justo id sapien 
+      porttitor feugiat. Nulla in ullamcorper neque, in sollicitudin risus. Nam blandit dui purus, 
+      porttitor molestie quam semper eu.
+    `,
+    image: null
+  },
+  {
+    title: "Distribution Prevention",
+    text: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque rutrum 
+      quis felis vulputate tristique. Integer lobortis, leo eu elementum sodales, 
+      dolor lacus ultricies velit, et condimentum dui lectus ut ex. Duis non tellus interdum, 
+      dictum ex et, faucibus nibh. Duis vitae est nec ligula aliquam varius non sed arcu. 
+      Integer faucibus ac augue sed mollis. Pellentesque nec eros quis justo venenatis rutrum. 
+      Maecenas eget augue gravida, ornare sem quis, bibendum risus. Sed mattis justo id sapien 
+      porttitor feugiat. Nulla in ullamcorper neque, in sollicitudin risus. Nam blandit dui purus, 
+      porttitor molestie quam semper eu.
+    `,
+    image: {
+      src: girlWithLaptop,
+      imagePosRight: false,
+      imageAltText: "Girl with laptop"
+    }
+  },
+  {
+    title: "",
+    text: ``,
+    image: null
+  }
+];
+
+
+export default featuresData;
