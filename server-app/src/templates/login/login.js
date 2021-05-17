50de17ad45f054c92c9b4ab0a7779bc924dc057d
gbe <gabriela.benejam@valtech.com>
2021-05-17 11:09:47 +0200

    Adding new changes for styling

--- server-app/src/templates/login/login.js
+++ server-app/src/templates/login/login.js
@@ -6,7 +6,10 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Alert from 'react-bootstrap/Alert'
 import axios from "axios";
+
+import classes from './login.module.css';
 
 class Login extends Component {
   state = {
@@ -15,6 +18,7 @@ class Login extends Component {
   };
 
   loginHandler = (event) => {
+    // todo
     event.preventDefault();
     console.log(this.state);
     axios
@@ -32,39 +36,48 @@ class Login extends Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col xs={6} md={4}></Col>
-          <Col xs={6} md={4}>
-            <Form onSubmit={this.loginHandler}>
-              <Form.Group controlId="formGroupEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter your email"
-                  value={this.state.email}
-                  onChange={(event) =>
-                    this.setState({ email: event.target.value })
-                  }
-                />
-              </Form.Group>
-              <Form.Group controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={(event) =>
-                    this.setState({ password: event.target.value })
-                  }
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-            <Link to="/new-user">New user?</Link>
+        <Row className={classes.CenteredHorizontally} >
+          <Col xs={6} md={6}>
+            <div className={classes.LoginContainer} >
+              <h1>Log in</h1>
+            </div>
+            <div className={`${classes.LoginForm} ${classes.LoginContainer}`} >
+              <Form onSubmit={this.loginHandler}>
+                <Form.Group className={classes.FormGroup} controlId="formGroupEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your email"
+                    value={this.state.email}
+                    onChange={(event) =>
+                      this.setState({ email: event.target.value })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group className={classes.FormGroup} controlId="formGroupPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={(event) =>
+                      this.setState({ password: event.target.value })
+                    }
+                  />
+                </Form.Group>
+                <Button className={classes.LoginButton} variant="primary" type="submit">
+                  Sign in
+                </Button>
+              </Form>
+            </div>
+            <div className={classes.LoginContainer} >
+              <p>New professor? <Link to="/new-user">Sign up</Link></p>
+              <Alert variant='secondary'>
+                If you are a student, your teacher should have given you your user/password
+                to be able to log in.
+              </Alert>
+            </div>
           </Col>
-          <Col xs={6} md={4}></Col>
         </Row>
       </Container>
     );
--- server-app/src/templates/newuser/NewUser.js
+++ server-app/src/templates/newuser/NewUser.js
@@ -5,6 +5,8 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
+import classes from './NewUser.module.css';
+
 class NewUser extends Component {
   state = {
     name: "",
@@ -16,43 +18,54 @@ class NewUser extends Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col xs={6} md={2}></Col>
-          <Col xs={6} md={8}>
+        <Row className={classes.CenteredHorizontally} >
+          <Col xs={8} md={8}>
+            <div className={classes.Container} >
               <h1>New user details</h1>
-            <Form>
+            </div>
+            <Form className={`${classes.Container} ${classes.RegisterForm}`} >
               <Form.Group controlId="formGroupEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridName">
+              <Form.Row className={classes.FormRow} >
+                <Form.Group as={Col} controlId="formGridName" className={classes.FormColumn} >
                   <Form.Label>Name</Form.Label>
                   <Form.Control type="text" placeholder="Enter name" />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridUsername">
+                <Form.Group as={Col} controlId="formGridUsername" className={classes.FormColumn} >
                   <Form.Label>Username</Form.Label>
                   <Form.Control type="text" placeholder="@Username" />
                 </Form.Group>
               </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridPassword1">
+              <Form.Row className={classes.FormRow} >
+                <Form.Group as={Col} controlId="formGridPassword1" className={classes.FormColumn} >
                   <Form.Label>Password:</Form.Label>
                   <Form.Control type="password" placeholder="Enter password" />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Group as={Col} controlId="formGridPassword" className={classes.FormColumn} >
                   <Form.Label>Repeat password:</Form.Label>
                   <Form.Control type="password" placeholder="Please re-enter password" />
                 </Form.Group>
               </Form.Row>
-              <Button variant="primary" type="submit">
+              <Form.Row className={classes.FormRow} >
+                <Form.Group as={Col} controlId="formGridCompany" className={classes.FormColumn} >
+                  <Form.Label>Company:</Form.Label>
+                  <Form.Control type="text" placeholder="Company you represent" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridCompanyCode" className={classes.FormColumn} >
+                  <Form.Label>Company code:</Form.Label>
+                  <Form.Control type="number" placeholder="Numeric code provided by company" />
+                </Form.Group>
+              </Form.Row>
+              <Button className={classes.RegisterButton} variant="primary" type="submit">
                 Submit
               </Button>
             </Form>
           </Col>
-          <Col xs={6} md={2}></Col>
         </Row>
       </Container>
     );
