8052559a8d8325bf36f6f65370552853200217d6
gbe <gabriela.benejam@valtech.com>
2021-05-17 20:00:41 +0200

    Moving client to main project to make docking easier

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+const dbHostName = process.env.DB_HOST || "127.0.0.1"
+const dbPort = process.env.DB_PORT || "27018"
 
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb://127.0.0.1:27018/smarteval', {
+mongoose.connect(`mongodb://${dbHostName}:${dbPort}/smarteval`, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
